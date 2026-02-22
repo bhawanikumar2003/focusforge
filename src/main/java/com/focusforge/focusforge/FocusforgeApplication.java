7f482a3e38f0042f6df456623918805d16ad6e48
User <user@example.com>
2026-02-22 14:59:02 +0530

    Initial commit

---
+++ src/main/java/com/focusforge/focusforge/FocusforgeApplication.java
@@ -0,0 +1,13 @@
+package com.focusforge.focusforge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FocusforgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FocusforgeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/focusforge/focusforge/FocusforgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.focusforge.focusforge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FocusforgeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
